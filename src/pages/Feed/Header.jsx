49334dbf7e398d578375d0ef267c8cf3718b9416
fbelam <fabricio.novaes@hotmail.com>
2024-12-12 00:45:44 -0300

    fix page feed

--- src/pages/Feed/Header.jsx
+++ src/pages/Feed/Header.jsx
@@ -3,18 +3,19 @@ import { HeaderST } from "./EstilosHeader";
 import { PrimeiraParte } from "./EstilosHeader";
 import { SegundaParte, Botao } from "./EstilosHeader";
 import LOGO from "../../assets/LOGO.png"
+import logo from "../../assets/logo.svg";
+
 export default function Header() {
 
   return (
     <div>
         <HeaderST>
                 <PrimeiraParte>
-                    <img src={LOGO} alt="logo" />
+                    <img src={logo} alt="logo" />
                 </PrimeiraParte>
                 <SegundaParte>
                     <a href="#">SOBRE</a>
-                    <a href="#">BUSINESS</a>
-                    <Botao > <a href="/login">LOGIN</a> </Botao>
+                     <Botao > <a href="/login">LOGIN</a> </Botao>
                     <Botao style={{ backgroundColor: '#FFEA96'}}> <a href="/cadastrar-senha">CADASTRAR AQUI</a></Botao>
                 </SegundaParte>
                 <div className='SegundaMetade'></div>
